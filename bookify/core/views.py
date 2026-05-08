233e3a1fb289415c8959d054b4146de11cd74e90
Badr Rafik <badr.rafik2006@gmail.com>
2026-05-08 18:54:52 +0300

    Fixed Borrow

--- bookify/core/views.py
+++ bookify/core/views.py
@@ -14,6 +14,9 @@ def get_role(request):
     except User.DoesNotExist:
         return ''
 
+def is_user_authenticated(request):
+    return bool(request.session.get('user_id'))
+
 def signup_page(request):
     error = None
     if request.method == 'POST':
@@ -35,7 +38,7 @@ def signup_page(request):
             User.objects.create(username=username, email=email, password=hashed_password)
             return redirect('login')
             
-    return render(request, 'sign-up.html', {'error': error, 'role': get_role(request)})
+    return render(request, 'sign-up.html', {'error': error, 'role': get_role(request), 'is_authenticated': is_user_authenticated(request)})
 
 def login_page(request):
     error = None
@@ -59,26 +62,26 @@ def login_page(request):
         else:
             error = 'Invalid username or password.'
 
-    return render(request, 'login.html', {'error': error, 'role': get_role(request)})
+    return render(request, 'login.html', {'error': error, 'role': get_role(request), 'is_authenticated': is_user_authenticated(request)})
 
 def home(request):
-    return render(request, 'home.html', {'role': get_role(request)})
+    return render(request, 'home.html', {'role': get_role(request), 'is_authenticated': is_user_authenticated(request)})
 
 def logout_user(request):
     request.session.flush()
     return redirect('login')
 
 def add_book_page(request):
-    return render(request, 'add_book.html', {'role': get_role(request)})
+    return render(request, 'add_book.html', {'role': get_role(request), 'is_authenticated': is_user_authenticated(request)})
 
 def book_details_page(request):
-    return render(request, 'book_details.html', {'role': get_role(request)})
+    return render(request, 'book_details.html', {'role': get_role(request), 'is_authenticated': is_user_authenticated(request)})
 
 def books_page(request):
-    return render(request, 'books.html', {'role': get_role(request)})
+    return render(request, 'books.html', {'role': get_role(request), 'is_authenticated': is_user_authenticated(request)})
 
 def edit_book_page(request):
-    return render(request, 'edit_book.html', {'role': get_role(request)})
+    return render(request, 'edit_book.html', {'role': get_role(request), 'is_authenticated': is_user_authenticated(request)})
 
 def my_books_page(request):
-    return render(request, 'my_books.html', {'role': get_role(request)})
+    return render(request, 'my_books.html', {'role': get_role(request), 'is_authenticated': is_user_authenticated(request)})
